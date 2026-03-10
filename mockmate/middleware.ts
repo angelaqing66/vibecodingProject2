ce4573224c5550b3d7c5fcb5483206043e16809b
Vartika Tewari <vartikatewari@users.noreply.github.com>
2026-03-10 14:52:30 -0400

    feat(profile): implement Profile Creation and Edit UI (Fixes #4)

--- mockmate/middleware.ts
+++ mockmate/middleware.ts
@@ -1,15 +1,16 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware';
 
 export default withAuth({
-    pages: {
-        signIn: "/login",
-    },
-})
+  pages: {
+    signIn: '/login',
+  },
+});
 
 export const config = {
-    matcher: [
-        "/search/:path*",
-        "/profile-setup/:path*",
-        "/dashboard/:path*",
-    ]
-}
+  matcher: [
+    '/search/:path*',
+    '/profile-setup/:path*',
+    '/dashboard/:path*',
+    '/profile',
+  ],
+};
